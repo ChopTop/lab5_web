9303627e4e26c80a7eb8dcd38fe7f0c79f56eeab
ChopTop <egor.melentiew@gmail.com>
2020-06-20 03:21:06 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,10 +1,8 @@
-print("Hello world")
-from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+@app.route("/data_to")
 
-@app.route("/")
-def hello():
-  return " <html><head></head> <body> Hello World! </body></html>"
-
-if __name__ == "__main__":
-  app.run(host='127.0.0.1',port=5000)
+def data_to():
+  some_pars = {'user':'Ivan','color':'red'}
+  some_str = 'Hello my dear friends!'
+  some_value = 10
+  return render_template('simple.html',some_str = some_str, some_value = some_value,some_pars=some_pars)
