c9e633be11b1fb76b3fe4063ca429c9e361ef75c
ChopTop <egor.melentiew@gmail.com>
2020-06-20 10:01:43 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -4,9 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
-
 SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
 
